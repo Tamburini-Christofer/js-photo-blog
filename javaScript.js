0369eb3059ae1144dd3d465c6c50feb61662548b
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-17 13:53:07 +0200

    Creazione javaScript

---
+++ javaScript.js
@@ -0,0 +1,27 @@
+//Colleghiamo javaScript a html prendendo gli elementi 
+//Prendiamo il contenitore dove andranno le Card
+const contenitoreCard = document.getElementById("contenitoreMain");
+
+//Chiamiamo l'API 
+axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
+        //Creiamo una variabile per salvare le Cards
+        const cards = response.data;
+        console.log()
+        //Creiamo una variabile che possa contenere tutta la card
+        let post = "";
+        //Facciamo un ciclo per creare le Card
+        cards.forEach(card => {
+            post += `<div class="card">     
+                        <img class="pin" src="img/pin.svg" alt="pinBaccheca">
+                            <div class="img"> 
+                                    <img src=" ${card.url}" alt=" ${card.title}"> 
+                            </div>
+                            
+                            <div> 
+                                    <p> ${card.title} </p> 
+                                    <date> ${card.date} </date>
+                            </div>
+                    </div>`
+        })
+        contenitoreCard.innerHTML = post;
+});
