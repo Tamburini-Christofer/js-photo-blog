17dc43f6d90ba372513ae96ec6a127282b734f41
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-18 20:08:07 +0200

    Esercizio concluso

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,14 @@
 //Colleghiamo javaScript a html prendendo gli elementi 
 //Prendiamo il contenitore dove andranno le Card
 const contenitoreCard = document.getElementById("contenitoreMain");
+//Prendiamo tutte le carte
+const immagine = document.querySelectorAll(".card img");
+//Prendiamo il contenitore che conterrà l'overplay
+const overlayDisplay = document.getElementById("immagineOver");
+//Prendiamo l'immagine in OverPlay
+const overlayImg = document.getElementById("overlayImg"); 
+//Prendiamo il bottone reset
+const bottone = document.getElementById("reset");
 
 //Chiamiamo l'API 
 axios.get("https://lanciweb.github.io/demo/api/pictures/")
@@ -13,32 +21,32 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
         cards.forEach(card => {
             post += `
              <div class="card">     
-                        <img class="pin" src="img/pin.svg" alt="pinBaccheca">
-                            <div class="img"> 
-                                    <img src=" ${card.url}" alt=" ${card.title}"> 
-                            </div>
-
-                            <div> 
-                                    <h4> ${card.title} </h4> 
-                                    <span> ${card.date} </span>
-                            </div>
-                    </div>`
+                      <img class="pin" src="img/pin.svg" alt="pinBaccheca">
+                        <div class="img"> 
+                      <img src=" ${card.url}" alt=" ${card.title}"> 
+                  </div>
+                  <div> 
+                         <h4> ${card.title} </h4> 
+                      <span> ${card.date} </span>
+                  </div>
+              </div>`
         })
+
+        //Scriviamo in post le card
         contenitoreCard.innerHTML = post;
 
-        const immagine = document.querySelectorAll(".card img");
-        const overlayDisplay = document.getElementById("immagineOver");
-        const bottone = document.getElementById("reset");
-        const overlayImg = document.getElementById("overlayImg"); 
-
+        //Creiamo un ForEach che possa ciclare le immagini per selezionarle
         immagine.forEach(card => {
+          //Aggiungiamo un evento click alle card
           card.addEventListener("click", () => {
+            //Associamo le sorgenti di card alle sorgenti dell'overplay
             overlayImg.src = card.src; 
             overlayImg.alt = card.alt; 
+             //Il contenitore Overplay diventa Flex
             overlayDisplay.style.display = "flex";
             });
         });
-
+         //Al click del bottone diventa none
         bottone.addEventListener("click", () => {
             overlayDisplay.style.display = "none";
         });
